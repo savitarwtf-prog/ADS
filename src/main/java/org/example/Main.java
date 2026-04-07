599ee8ecd73750145689b4a2ab1b13f288a41718
aldi2 <aldik@gmail.com>
2026-04-07 13:39:04 +0500

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        testArrayList();
+        System.out.println("---------------------------");
+        testLinkedList();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    public static void testArrayList() {
+        System.out.println("Testing MyArrayList:");
+        MyList<Integer> list = new MyArrayList<>();
+
+        // Тестируем add и size
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println("Size after adding 3 elements: " + list.size());
+
+        // Тестируем add(index) и get
+        list.add(1, 15);
+        System.out.println("Element at index 1: " + list.get(1)); // Должно быть 15
+
+        // Тестируем remove
+        list.remove(2);
+        System.out.println("Size after removal: " + list.size());
+        System.out.println("Element at index 2 now: " + list.get(2)); // Должно быть 30
+
+        // Тестируем exists и indexOf
+        System.out.println("Exists 15? " + list.exists(15));
+        System.out.println("Index of 30: " + list.indexOf(30));
+
+        // Тестируем sort
+        list.add(5);
+        list.sort();
+        System.out.print("Sorted list: ");
+        for (Integer i : list) {
+            System.out.print(i + " ");
         }
+        System.out.println();
+    }
+
+    public static void testLinkedList() {
+        System.out.println("Testing MyLinkedList:");
+        MyList<String> list = new MyLinkedList<>();
+
+        // Тестируем addFirst и addLast
+        list.addLast("Middle");
+        list.addFirst("Start");
+        list.addLast("End");
+
+        System.out.println("First element: " + list.getFirst());
+        System.out.println("Last element: " + list.getLast());
+
+        // Тестируем removeFirst и removeLast
+        list.removeFirst();
+        System.out.println("New first after removeFirst: " + list.getFirst());
+
     }
 }
--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -1,4 +1,194 @@
 package org.example;
 
-public class MyLinkedList {
+import java.util.Iterator;
+
+public class MyLinkedList<T> implements MyList<T> {
+    private class MyNode {
+        T data;
+        MyNode next;
+        MyNode prev;
+
+        MyNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+
+    @Override
+    public void add(T item) {
+        MyNode newNode = new MyNode(item);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        MyNode newNode = new MyNode(item);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    private MyNode getNode(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        MyNode current = head;
+        for (int i = 0; i < index; i++) current = current.next;
+        return current;
+    }
+
+    @Override
+    public T get(int index) {
+        return getNode(index).data;
+    }
+
+    @Override
+    public T getFirst() {
+        return head != null ? head.data : null;
+    }
+
+    @Override
+    public T getLast() {
+        return tail != null ? tail.data : null;
+    }
+
+    @Override
+    public void remove(int index) {
+        MyNode nodeToRemove = getNode(index);
+        if (nodeToRemove.prev != null) nodeToRemove.prev.next = nodeToRemove.next;
+        else head = nodeToRemove.next;
+
+        if (nodeToRemove.next != null) nodeToRemove.next.prev = nodeToRemove.prev;
+        else tail = nodeToRemove.prev;
+
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        if (size > 0) remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        if (size > 0) remove(size - 1);
+    }
+
+    @Override
+    public void set(int index, T item) {
+        getNode(index).data = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index == 0) addFirst(item);
+        else if (index == size) addLast(item);
+        else {
+            MyNode current = getNode(index);
+            MyNode newNode = new MyNode(item);
+            newNode.prev = current.prev;
+            newNode.next = current;
+            current.prev.next = newNode;
+            current.prev = newNode;
+            size++;
+        }
+    }
+
+    @Override
+    public void sort() {
+        // Простая сортировка пузырьком для примера
+        if (size > 1) {
+            for (int i = 0; i < size; i++) {
+                MyNode current = head;
+                while (current.next != null) {
+                    if (((Comparable<T>) current.data).compareTo(current.next.data) > 0) {
+                        T temp = current.data;
+                        current.data = current.next.data;
+                        current.next.data = temp;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        MyNode current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.data.equals(object)) return i;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        MyNode current = tail;
+        for (int i = size - 1; i >= 0; i--) {
+            if (current.data.equals(object)) return i;
+            current = current.prev;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        MyNode current = head;
+        for (int i = 0; i < size; i++) {
+            result[i] = current.data;
+            current = current.next;
+        }
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private MyNode current = head;
+            @Override
+            public boolean hasNext() { return current != null; }
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
 }
